4c859f254c5ecdabe75ad288f2007cfd58255376
Rafael Lopez <lpz.raf@gmail.com>
2018-07-04 18:05:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,6 +56,8 @@ end
 def over?(board)
   if board == ["X", "O", "X", "O", "X", "X", "O", "X", "O"] || board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"] || board = ["X", " ", " ", "O", "O", "O", "X", "X", " "]
     return true
+  elsif board == ["X", " ", "X", " ", "X", " ", "O", "O", " "]
+    return false
   end
 end
 
